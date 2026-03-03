e4c3e96bca0a5d44e63d218d72bcd208f1fd701a
Daserilz <knjirapat@gmail.com>
2026-03-03 15:41:23 +0700

    Update index.ts

--- src/app/images/index.ts
+++ src/app/images/index.ts
@@ -1,10 +1,12 @@
 // 1. นำเข้าไฟล์รูปภาพมาเก็บในตัวแปร
 import UE_SciFiWeaponShop from './UE_SciFiWeaponShop.png';
 import g from './g.png';
+import AiVSAlien from './AiVSAlien.png';
 
 // 2. ส่งออก (Export) ในรูปแบบ Object ชื่อ Assets ตามที่ Portfolio.tsx เรียกใช้
 export const Assets = {
   UE_SciFiWeaponShop,
   g,
+  AiVSAlien,
   // เพิ่มรูปอื่นๆ เข้าไปที่นี่...
 };
