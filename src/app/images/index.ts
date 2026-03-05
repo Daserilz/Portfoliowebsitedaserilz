05e3e98e63ba1d94d5fe7cb66504def1ad1d6b2e
Daserilz <knjirapat@gmail.com>
2026-03-05 12:59:36 +0700

    Update index.ts

--- src/app/images/index.ts
+++ src/app/images/index.ts
@@ -1,5 +1,6 @@
 // 1. Import ไฟล์รูปภาพทั้งหมดที่อยู่ในโฟลเดอร์นี้
 import AiVSAlien from './AiVSAlien.png';
+import AivsAlienGame from './AivsAlienGame.png';
 import DemonAnim from './DemonAnim.png';
 import TryNot from './TryNot.png';
 import UE_SciFiWeaponShop from './UE_SciFiWeaponShop.png';
@@ -8,6 +9,7 @@ import g from './g.png';
 // 2. Export ออกไปเป็น Object ชื่อ Assets ตามที่ Portfolio.tsx เรียกใช้
 export const Assets = {
   AiVSAlien,
+  AivsAlienGame,
   DemonAnim,
   TryNot,
   UE_SciFiWeaponShop,
